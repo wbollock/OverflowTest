30bd61cb70732a6ee8217797501d1d44b6154db7
Will Bollock <wbollock@gmail.com>
2018-05-20 17:42:28 -0400

    Initial commit

---
+++ app/src/main/java/wbollock/com/overflowtest/MainActivity.java
@@ -0,0 +1,20 @@
+package wbollock.com.overflowtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
